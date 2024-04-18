bd12b17a653f5f5c39fad3510beec30657474ec2
Mil0w0 <lhilderal@myges.fr>
2024-04-18 09:55:16 +0200

    adding footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
+import "../styles/Footer.css";
+import Logo from "../images/logo.png";
 export function Footer() {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
-    );
+        <footer>
+            <div className="green-line"></div>
+            <div className="footer-content">
+                <img src={Logo} alt={"logo"} />
+                <div className="raccourcis">
+                    <a href="/">Mentions légales</a>
+                    <a href="/">CGU</a>
+                    <a href="/">Contact</a>
+                </div>
+               <div>{"2024"}</div>
+                <p>Ce site est destiné aux gérants d'association orientée médicale souhaitant créer un site web et l'heberger rapidement. Arcadia Solutions. Il s’agit d’une entreprise fictive. By HILDERAL - HIRSCH - RUTH @ ESGI PARIS
+                </p>
+            </div>
+        </footer>
+    )
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,22 @@
 import Header from "../components/Header";
-// import '../styles/Home.css';
+import '../styles/Home.css';
+import Logo from '../images/logo-black.svg';
+import {Button} from "@mui/material";
+import {Footer} from "../components/Footer";
 
 export function Home() {
 
         return (
             <div>
                 <Header />
-                <h1>Créer le site pour votre association 100 % personalisable en <span className="green">5 minutes</span> !</h1>
-            </div>
+                <div id="section-presentation">
+                    <img src={Logo} alt={"logo"} />
+                    <div>
+                        <h1>Créer le site pour votre association 100 % personalisable en <span className="green">5 minutes</span> !</h1>
+                        <Button variant="contained" color="primary">Créer un site</Button>
+                    </div>
+                </div>
+                <Footer />
+                </div>
         );
 }
