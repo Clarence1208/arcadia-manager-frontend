25d8f56348f4ffd180b48e13fa84a41aaa286034
Nino <ninoplane@gmail.com>
2024-07-21 13:39:42 +0200

    contact css v2

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import {Footer} from "./components/Footer";
+import Header from "../components/Header";
+import {Footer} from "../components/Footer";
 import {Button, TextField} from "@mui/material";
 import '../styles/Contact.css';
 
